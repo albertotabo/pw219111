354235f1d226e23582c62f3cefbddf30c1cc8e6b
albertotabo <jesuschivas26@gmail.com>
2019-04-29 12:03:22 -0600

    Conexion a la base de datos

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,14 +1,35 @@
+const path = require('path');
 const express = require('express');
+const morgan= require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 const app = express();
+
 //importar Rutas
 const indiceRutas =require('./rutas/index');
+
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 //Usamos las rutas
 app.use("/",indiceRutas);
 
 
+//Middleware
+
+app.use(morgan('dev')); //dev= desarrollo
+app.use(myConnection(mysql,{
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'crudenodejsmysql'
+},'single'));
+
 //Activar el servidor de escucha
 
-app.listen(3000, () => {
+app.listen(app.get('port'), () => {
 	console.log("Escuchando en puerto 3000, jajajaj");
 });
 
--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -3,7 +3,7 @@ const rutas = express.Router();
 
 //GET es lo que solicita el navegador del cliente
 rutas.get("/",(req,res) => {
-	res.send("Hola Mundo Node, soy genial atte : Tu mom");
+	res.send("Hola Mundo Node, soy genial atte : Tu mama");
 
 });
 module.exports = rutas;
