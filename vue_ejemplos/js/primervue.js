d6b2e983482576d3fcfffcfb16097d84a202a94d
albertotabo <jesuschivas26@gmail.com>
2019-05-07 12:03:50 -0600

    Primer Vue con html

---
+++ vue_ejemplos/js/primervue.js
@@ -0,0 +1,13 @@
+const app = new	Vue({
+	el: "#app",
+	data: {
+		titulo: 'Hola Mundo con VUE',
+		frutas: ['manzana','peras','platano','mango']
+		frutas2: [
+			{nombe:'manzana',cantidad: 10},
+			{nombe:'peras',cantidad: 0},
+			{nombe:'platanos',cantidad: 20},
+			{nombe:'mangos',cantidad: 80}
+		]
+	}
+}); 
