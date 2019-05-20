ae1c5db21ebef6e9e8dc4290f54c4a061a48fe9d
albertotabo <jesuschivas26@gmail.com>
2019-05-20 02:22:21 -0500

    Ahi la llevamos

---
+++ ciclodevidavue/js/ciclodevida.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		saludo: 'Hola Vue'
+	},
+	beforeCreate(){
+		console.log('Antes de crear el objeto vue');
+	},
+	created(){
+		console.log('El Objeto vue fue creado');
+	},
+	beforeMount(){
+		console.log('El DOM no se ha cargado');
+
+	},
+	mounted(){
+		console.log('La pagina o DOM ha sido cargada');
+	},
+	beforeUpdate(){
+		console.log('Antes de Actualizar cualquier elemento de la pagina');
+	},
+	updated(){
+		console.log('La pagina ha sido actualizada');
+	},
+	beforeDestroy(){
+		console.log('Antes de destruir el objeto Vue');
+	},
+	destroyed(){
+		console.log('El objeto Vue ha sido destruido');
+	},
+	methods:{
+		destruir(){
+			this.$destroy();
+		}
+	}
+})
--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -8,6 +8,28 @@ const app = new	Vue({
 			{nombe:'peras',cantidad: 0},
 			{nombe:'platanos',cantidad: 20},
 			{nombe:'mangos',cantidad: 80}
-		]
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods: {
+		agregarFruta(){
+			//console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			})
+			this.nuevaFruta = '';
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0;
+			for(fruta of this.frutas2){
+				this.total = this.total+fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 }); 
